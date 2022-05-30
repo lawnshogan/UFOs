ec77c07eb7e151d2dc5a4f203a8471a0b1e1f521
lawnshogan <Shawnlogan.gis@gmail.com>
2022-05-30 14:23:03 -0600

    Module 11 - 11.5.2

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,18 @@
 const tableData = data;
 // Reference the HTML table by using d3 (JavaScript Library)
 var tbody = d3.select("tbody");
+// Create a function that iterates through the array of objects and appends to table
+// tbody.html("") tells us to reference html page and ("") is an empty string
+function buildTable(data) {
+    tbody.html("");
+// Add the forEach Function to iterate through data
+// let row = tbody.append("tr"); create variable that will append a row to the table
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) => { //Loop through data rows telling JavaScript to reference one object from the array of UFO sightings
+            let cell = row.append("td"); // Append data to a table
+            cell.text(val); // Add Values to table
+            }
+        );
+    });
+}
